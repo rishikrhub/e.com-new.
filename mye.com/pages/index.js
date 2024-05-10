6185968fa0a8c8d428ff1fd54d2bfface911ed08
rishikrhub <119436665+rishikrhub@users.noreply.github.com>
2024-05-10 14:16:26 +0000

    og

--- mye.com/pages/index.js
+++ mye.com/pages/index.js
@@ -2,8 +2,9 @@ import Nav from "@/components/Nav";
 export default function Home() {
   return (
        <div
-       className="bg-blue-900 min-h-screen">
+       className="bg-blue-900 min-h-screen flex">
          <Nav/>
+         <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4"></div>
        </div>
   );
 }
