f0c24be41f262b7e29770365b6771de1ffb223d6
rishikrhub <119436665+rishikrhub@users.noreply.github.com>
2024-05-13 11:57:08 +0000

    okkkk

--- mye.com/pages/products/new.js
+++ mye.com/pages/products/new.js
@@ -1,12 +1,35 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProducts() {
+  const [title,setTitle] = useState('');
+  const [description,setDescription] = useState('');
+  const [price,setPrice] = useState('');
+  async function createProduct() {
+    const data = {title,description,price};
+    await axios.post('/api/products', );
+  }
     return (
        <Layout>
+        <form onSubmit={createProduct}>
+        </form>
         <h1>New Products</h1>
-         <input type="text" placeholder="product name"/>
-         <textarea placeholder="description"></textarea>
-         
+        <label>Product name</label>
+         <input type="text" 
+         placeholder="product name" 
+         value={title} 
+         onChange={ev => setTitle(ev.target.value)}/>
+         <label>Description</label>
+         <textarea placeholder="description" 
+         value={description} 
+         onChange={ev => setTitle(ev.target.value)}/>
+         <label>Price (in INR)</label>
+         <input type="number" 
+         placeholder="price" 
+         value={price} 
+         onChange={ev => setTitle(ev.target.value)}/>
+         <button type="submit" 
+         className="btn-primary">Save</button>
        </Layout>
     );
 }
